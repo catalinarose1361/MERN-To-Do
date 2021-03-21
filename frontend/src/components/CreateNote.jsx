801c2010cb50c951e7eeed97a84e500e830f4859
Catalina Williams <catalina@Catalinas-iMac.local>
2021-03-21 12:12:14 -0600

    created all components

---
+++ frontend/src/components/CreateNote.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function CreateNote() {
+    return <div>
+        <h1>Create Note</h1>
+    </div>
+}
+
+export default CreateNote
---
+++ frontend/src/components/Notes.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Notes() {
+    return <div>
+        <h1>Notes</h1>
+    </div>
+}
+
+export default Notes
