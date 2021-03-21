947f4a05b7041953259e8f67a69cf6bc7cab5dac
Catalina Williams <catalina@Catalinas-iMac.local>
2021-03-21 12:05:32 -0600

    created routes with react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import {} from "react-router-dom"
+import {BrowserRouter as Router, Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
+
 function App() {
-  return <Navbar />
-  //Home
-  //Notes
-  //Create Note
+  return (
+    <Router>
+      // navbar is outside Route because it shows on every page
+       <Navbar />
+
+        <Route path="/">
+        //Home
+        </Route>
+
+        <Route path="/notes">
+        //Notes
+        </Route>
+
+        <Route path="/create">
+        //Create Note
+        </Route>
+       
+    </Router>
+  )
+  
+
 }
 
 export default App;
