0aaa2bf574817e5c4341565293fbe150af47768c
Catalina Williams <catalina@Catalinas-iMac.local>
2021-03-30 10:05:06 -0600

    imported components to be used in App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Navbar from "./components/Navbar"
+import Notes from "./components/Notes"
+import Home from "./components/Home"
+import CreateNote from "./components/CreateNote"
 
 function App() {
   return (
