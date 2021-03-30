eebee539e13c03ceacbf1e088d18d3ce3d3e98f4
Catalina Williams <catalina@Catalinas-iMac.local>
2021-03-30 10:50:19 -0600

    built create note form using bootstrap

--- frontend/src/components/CreateNote.jsx
+++ frontend/src/components/CreateNote.jsx
@@ -3,6 +3,20 @@ import React from "react"
 function CreateNote() {
     return <div className="container">
         <h1>Create Note</h1>
+        
+        <form>
+            <div className="form-group">
+                <input name="title" className="form-control" autoComplete="off" placeholder="Note Title"></input>  
+            </div>
+
+            <div className="form-group">
+                <textarea name="content" className="form-control" autoComplete="off" placeholder="Note Content"></textarea>  
+            </div>
+            
+            <button clasName="btn btn-lg btn-info">Add Note</button>
+
+        </form>
+
     </div>
 }
 
