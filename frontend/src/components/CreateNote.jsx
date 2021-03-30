bab2d41d09c2bf9303fc4f1a6b499ee871c5a428
Catalina Williams <catalina@Catalinas-iMac.local>
2021-03-30 11:12:10 -0600

    created handleChange function that saves the values of the form in state

--- frontend/src/components/CreateNote.jsx
+++ frontend/src/components/CreateNote.jsx
@@ -1,19 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
+
+
 
 function CreateNote() {
+    const [input, setInput] = useState({
+        title: '',
+        content: ''
+    })
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setInput(prevInput => {
+            return {
+                ...prevInput,
+                [name]: value
+            }
+        })
+    }
+
     return <div className="container">
         <h1>Create Note</h1>
         
         <form>
             <div className="form-group">
-                <input name="title" className="form-control" autoComplete="off" placeholder="Note Title"></input>  
+                <input onChange={handleChange} name="title" value={input.title} className="form-control" autoComplete="off" placeholder="Note Title"></input>  
             </div>
 
             <div className="form-group">
-                <textarea name="content" className="form-control" autoComplete="off" placeholder="Note Content"></textarea>  
+                <textarea onChange={handleChange} name="content" value={input.content} className="form-control" autoComplete="off" placeholder="Note Content"></textarea>  
             </div>
             
-            <button clasName="btn btn-lg btn-info">Add Note</button>
+            <button onClick={handleClick} clasName="btn btn-lg btn-info">Add Note</button>
 
         </form>
 
