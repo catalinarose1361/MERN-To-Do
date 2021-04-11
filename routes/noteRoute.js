dfd413e427fea97ec4f1965e5d9184e9a4600df8
Catalina Williams <webdevcatalina@gmail.com>
2021-04-11 14:48:57 -0600

    added descriptive comments for get request function in note Route.js

--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Note = require("../models/noteModel")
-
+// taking the data posted to the route and saving it on the back end
 router.route("/create").post((req, res) => {
     const title = req.body.title;
     const content = req.body.content;
@@ -12,8 +12,9 @@ router.route("/create").post((req, res) => {
 
     newNote.save();
 })
-
+//sends data to Notes page 
 router.route("/notes").get((req, res) => {
+    // takes the Note model and calls the find method
     Note.find()
         .then(foundNotes => res.json(foundNotes))
 })
