fcb89d13f43b07adf0b8caa9c8cafe9e1b1cbda2
Catalina Williams <webdevcatalina@gmail.com>
2021-04-11 15:04:46 -0600

    added code to notes component that fetches json data from the notes route and saves it to state to be used and displayed on the front end

--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,8 +1,30 @@
-import React from "react"
-
+import React, {useEffect, useState} from "react"
+//fetch data from notes route
 function Notes() {
+    const [notes, setNotes] = useState([{
+        title: "",
+        content: ""
+    }])
+
+    //the useEffect method fetches the data
+    useEffect(() => {
+        fetch("/notes").then(res => {
+            if(res.ok) {
+                return res.json()
+            }
+            // and then sets the response equal to setNotes in State
+        }).then(jsonRes => setNotes(jsonRes))
+    })
+
     return <div className="container">
         <h1>Notes</h1>
+        {notes.map(note => 
+        <div>
+            <h1>{note.title}</h1>
+            <p>{note.content}</p>
+        </div>
+        )}
+
     </div>
 }
 
