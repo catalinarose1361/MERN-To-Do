cda7f876c85a7890734498881b1084f9d5ed9dd9
Catalina Williams <webdevcatalina@gmail.com>
2021-04-18 12:10:52 -0600

    application ready to push to heroku

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use(express.json())
 mongoose.connect("mongodb+srv://catalina-admin:fliaJUchyWudg52g@cluster0.xttdr.mongodb.net/notesDB")
 
 app.use("/", require("./routes/noteRoute"))
+//if the app is in Production mode use the 
+//static files located in the build folder
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(3001, function() {
     console.log("express server is running on port 3001")
